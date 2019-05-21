9f64420a922545f53fc95e7efc10a06a67273201
Johannes Maeß <johannes.maess@ibm.com>
2019-05-21 18:55:39 +0800

    Added (mockup) tables to Statistics and Testing page, moved to new API for Statistics data - supplement to latest commit

---
+++ client/src/components/SimpleTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <table class="pure-table pure-table-bordered">
+    <thead>
+      <tr>
+        <th v-for="(field, index) in head" :key="index">{{field}}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(row, index) in body" :key=index>
+        <td v-for="(field, index) in row" :key="index">{{field}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'SimpleTable',
+    props: {
+      content: Array
+    },
+    computed: {
+      head: function () {
+        return this.content[0]
+      },
+      body: function () {
+        return this.content.slice(1)
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
